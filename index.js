bc2eb38a574941e26be53aac108ff00b21ae72a5
rwed <65749968+rwedmeow@users.noreply.github.com>
2023-11-07 19:50:26 +0300

    Node server

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const http = require("http");
-const PORT = '8080'
+// const http = require("http");
+// const PORT = '8080';
 
-http
-  .createServer((req, res) => {
-    /* handle http requests */
-  })
-  .listen(PORT);
+import getAllProduct from "../users";
+// let products = [
+//   { id: 1, name: "laptop", price: 3222 },
+//   { id: 2, name: "Webcap", price: 1233 },
+//   { id: 3, name: "Smart phone", price: 1233 }
+// ];
 
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
+// const handelServserError = (res, statusCode, message) => {
+//   res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+//   res.write(message);
+//   res.end();
+// };
+
+// http
+//   .createServer((req, res) => {
+//     /* handle http requests */
+//   })
+//   .listen(PORT);
+
+// console.log(`Server running at http://127.0.0.1:${PORT}/`);
+console.log('hello from backend');
